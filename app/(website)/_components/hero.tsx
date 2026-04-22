7c765fbe94d83f8d03ad62ed5af042f70a1e2bc1
HanazakiStudio <leonardo@hanazakistudio.com>
2026-04-22 17:20:10 -0300

    Update hero.tsx

--- app/(website)/_components/hero.tsx
+++ app/(website)/_components/hero.tsx
@@ -26,13 +26,8 @@ export function Hero() {
   }, [videoRef]);
 
   return (
-    <section className="w-full h-screen relative bg-hero-mobile bg-cover bg-center bg-no-repeat">
-      
-      {/* Overlay */}
-      <div className="absolute inset-0 z-10 bg-gradient-to-b from-[#0e0e0e]/70 via-[#0e0e0e]/70 to-[#0e0e0e]" />
-
-      {/* Video */}
-      <div className="hidden sm:block w-full h-full absolute inset-0">
+    <section className="w-full h-screen relative bg-hero-mobile bg-cover bg-center bg-no-repeat after:content-[''] after:w-full after:h-full after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:z-10 after:bg-gradient-to-b from-[#0e0e0e]/70 via-[#0e0e0e]/70 to-[#0e0e0e]">
+      <div className="hidden sm:block w-full h-full absolute top-0 left-0 right-0 bottom-0">
         <video
           ref={videoRef}
           loop
@@ -47,20 +42,18 @@ export function Hero() {
       </div>
 
       <div className="relative z-20 w-full h-full px-6 flex flex-col justify-center sm:px-16 bg-hero-glows bg-no-repeat bg-contain bg-center lg:container lg:mx-auto">
-        
         <motion.div
           initial="initial"
           animate="animate"
           variants={ContainerAnimation}
           className="relative w-full flex flex-col items-center justify-center space-y-6 sm:items-center"
         >
-          
           <motion.div
             variants={TagAnimation}
             className="flex items-center justify-center space-x-1 bg-gold-primary/25 px-4 py-1 rounded-md cursor-default"
           >
             <span className="uppercase poppins-font font-semibold !leading-none text-gold-primary text-sm text-center sm:mt-[2px] sm:text-base">
-              Para Incorporadoras de Alto Padrão
+              Mergulhe na Futura Arquitetura
             </span>
             <ArrowUpRight color="#B9A568" />
           </motion.div>
@@ -69,43 +62,51 @@ export function Hero() {
             variants={TitleAnimation}
             className="poppins-font text-2xl max-w-[275px] text-light-primary font-semibold text-center sm:text-5xl sm:max-w-none lg:text-7xl lg:leading-[1.1]"
           >
-            Salas imersivas que transformam visita em{" "}
+            Onde a busca pelo{" "}
             <strong className="font-semibold bg-gradient-to-b from-[#D1BC7D] to-[#534A2F] inline-block text-transparent bg-clip-text">
-              decisão de compra
-            </strong>
-            .
+              excepcional
+            </strong>{" "}
+            redefine o{" "}
+            <strong className="font-semibold bg-gradient-to-b from-[#D1BC7D] to-[#534A2F] inline-block text-transparent bg-clip-text">
+              padrão
+            </strong>{" "}
+            do mercado.
           </motion.h1>
 
           <motion.div
             variants={SubTitleAnimation}
-            className="w-full overflow-hidden flex flex-col items-center justify-center space-y-2 sm:flex-row sm:space-y-0 sm:space-x-4"
+            className="w-full overflow-hidden flex flex-col items-center justify-center space-y-2 sm:flex-row sm:space-y-0 sm:space-x-4 sm:before:content-[''] sm:before:h-[1px] sm:before:basis-full sm:before:bg-gold-primary sm:before:mr-4 after:content-[''] sm:after:h-[1px] sm:after:basis-full sm:after:bg-gold-primary sm:after:ml-4"
           >
-            
             <motion.p
               variants={SubTitleItemAnimation}
               className="manrope-font text-light-primary text-base lg:text-xl"
             >
-              Unreal Engine
+              Tecnologia
             </motion.p>
 
-            <motion.div className="h-6 w-[1px] bg-gold-primary lg:h-8" />
-
-            <motion.p
-              variants={SubTitleItemAnimation}
-              className="manrope-font text-light-primary text-base lg:text-xl"
-            >
-              Alto Padrão
-            </motion.p>
-
-            <motion.div className="h-6 w-[1px] bg-gold-primary lg:h-8" />
+            <motion.div
+              variants={SubTitleLineAnimation}
+              className="h-6 w-[1px] min-w-[1px] bg-gold-primary lg:h-8"
+            />
 
             <motion.p
               variants={SubTitleItemAnimation}
               className="manrope-font text-light-primary text-base lg:text-xl"
             >
-              Integrada ao Stand
+              Interatividade
             </motion.p>
 
+            <motion.div
+              variants={SubTitleLineAnimation}
+              className="h-6 w-[1px] min-w-[1px] bg-gold-primary lg:h-8"
+            />
+
+            <motion.p
+              variants={SubTitleItemAnimation}
+              className="min-w-[107px] manrope-font text-light-primary text-nowrap sm:min-w-[123px] text-base lg:min-w-[154px] lg:text-xl"
+            >
+              Realidade virtual
+            </motion.p>
           </motion.div>
         </motion.div>
       </div>
