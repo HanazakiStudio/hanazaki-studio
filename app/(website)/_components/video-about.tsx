b007c68042dbeb3879d300ba4f6691e93e328228
HanazakiStudio <leonardo@hanazakistudio.com>
2026-04-23 19:54:42 -0300

    Update video-about.tsx

--- app/(website)/_components/video-about.tsx
+++ app/(website)/_components/video-about.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { PlayCircle } from "lucide-react";
 import AboutImage from "@/public/images/below-hero-example.png";
-import { SyntheticEvent, useState } from "react";
+import { MouseEventHandler, SyntheticEvent, useState } from "react";
 
 import {
   LineAnimation,
@@ -29,11 +29,12 @@ export function VideoAbout() {
     <>
       <VideoModal isModalOpen={isModalOpen} CloseModal={CloseModal} />
 
+      {/* <section className="relative z-30 w-full aspect-video rounded-[30px] overflow-hidden flex items-center justify-center lg:rounded-[60px] after:content-[''] after:w-full after:h-full after:bg-gray-primary/60 after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:z-40"> */}
       <section className="relative z-30 w-full pt-[56.66%] rounded-[30px] overflow-hidden flex items-center justify-center lg:rounded-[60px] after:content-[''] after:w-full after:h-full after:bg-gray-primary/60 after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:z-40">
-        
+        {/* TODO: inserir a imagem por enquanto depois pegar e colocar o video */}
         <Image
           src={AboutImage}
-          alt="Experiência Hanazaki Studio"
+          alt="Exemplo"
           fill
           priority
           placeholder="blur"
@@ -45,42 +46,28 @@ export function VideoAbout() {
           onClick={OpenModal}
           initial="initial"
           whileInView="animate"
-          viewport={{ once: true, amount: 0.4 }}
+          viewport={{ once: true, amount: "some" }}
           variants={ContainerAnimation}
-          className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-50 flex flex-col items-center justify-center space-y-5 cursor-pointer group"
+          className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-50 flex flex-col space-y-6 items-center justify-center group cursor-pointer overflow-hidden"
         >
-          
-          {/* LINHA SUPERIOR */}
-          <motion.div
-            variants={LineAnimation}
-            className="w-16 h-[1px] bg-gold-primary opacity-70"
-          />
+          <motion.div variants={LineAnimation} className="w-full h-[2px] min-h-[2px] bg-gold-primary self-start" />
 
-          {/* TEXTO */}
           <motion.h2
             variants={TextAnimation}
-            className="text-center uppercase tracking-[0.12em] text-light-primary font-light text-xl sm:text-4xl lg:text-5xl"
+            className="text-2xl text-center uppercase text-light-primary font-medium sm:text-5xl lg:text-6xl"
           >
-            Dê vida às{" "}
-            <span className="text-gold-primary font-medium">
-              ideias
-            </span>
+            Dê vida às <strong className="font-medium text-gold-primary">ideias</strong>
           </motion.h2>
 
-          {/* PLAY */}
           <motion.div variants={PlayAnimation}>
             <PlayCircle
-              size="60px"
-              strokeWidth={1.2}
-              className="text-light-primary group-hover:text-gold-primary transition-all duration-300"
+              size="67px"
+              strokeWidth={1.5}
+              className="text-light-primary group-hover:text-gold-primary transition-colors"
             />
           </motion.div>
 
-          {/* LINHA INFERIOR */}
-          <motion.div
-            variants={LineAnimation}
-            className="w-16 h-[1px] bg-gold-primary opacity-70"
-          />
+          <motion.div variants={LineAnimation} className="w-full h-[2px] min-h-[2px] bg-gold-primary self-end" />
         </motion.div>
       </section>
     </>
