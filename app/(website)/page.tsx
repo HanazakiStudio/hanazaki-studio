757101afaaf5191f44f6785c8fca0c9fdf5b72aa
HanazakiStudio <leonardo@hanazakistudio.com>
2026-04-23 16:54:02 -0300

    Update page.tsx

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useEffect } from "react";
+
 import { Hero } from "@/app/(website)/_components/hero";
 import { VideoAbout } from "@/app/(website)/_components/video-about";
 import { About } from "@/app/(website)/_components/about";
@@ -5,13 +9,29 @@ import { Services } from "@/app/(website)/_components/services";
 import { Projects } from "@/app/(website)/_components/projects";
 
 export default function Home() {
+
+  useEffect(() => {
+    if (window.location.hash === "#projetos") {
+      setTimeout(() => {
+        const el = document.getElementById("projetos");
+        if (el) {
+          el.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 300);
+    }
+  }, []);
+
   return (
     <>
       <Hero />
       <VideoAbout />
       <About />
       <Services />
-      <Projects />
+
+      {/* IMPORTANTE: ID AQUI */}
+      <section id="projetos">
+        <Projects />
+      </section>
     </>
   );
 }
