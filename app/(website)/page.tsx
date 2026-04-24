db9ff4626d8beb7beedd537c6b0057fcd1a4bdf5
HanazakiStudio <leonardo@hanazakistudio.com>
2026-04-24 19:45:24 -0300

    Update page.tsx

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 
 import { Hero } from "@/app/(website)/_components/hero";
+import { LogosBanner } from "@/app/(website)/_components/logos-banner";
 import { VideoAbout } from "@/app/(website)/_components/video-about";
 import { About } from "@/app/(website)/_components/about";
 import { Services } from "@/app/(website)/_components/services";
@@ -24,6 +25,7 @@ export default function Home() {
   return (
     <>
       <Hero />
+      <LogosBanner />
       <VideoAbout />
       <About />
       <Services />
