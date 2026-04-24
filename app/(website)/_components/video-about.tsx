057004f08fb15aaeb97eec4f7bcc9e454581c6a3
HanazakiStudio <leonardo@hanazakistudio.com>
2026-04-24 11:46:23 -0300

    Update video-about.tsx

--- app/(website)/_components/video-about.tsx
+++ app/(website)/_components/video-about.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { PlayCircle } from "lucide-react";
 import AboutImage from "@/public/images/below-hero-example.png";
-import { MouseEventHandler, SyntheticEvent, useState } from "react";
+import { SyntheticEvent, useState } from "react";
 
 import {
   LineAnimation,
@@ -29,45 +29,60 @@ export function VideoAbout() {
     <>
       <VideoModal isModalOpen={isModalOpen} CloseModal={CloseModal} />
 
-      {/* <section className="relative z-30 w-full aspect-video rounded-[30px] overflow-hidden flex items-center justify-center lg:rounded-[60px] after:content-[''] after:w-full after:h-full after:bg-gray-primary/60 after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:z-40"> */}
-      <section className="relative z-30 w-full pt-[56.66%] rounded-[30px] overflow-hidden flex items-center justify-center lg:rounded-[60px] after:content-[''] after:w-full after:h-full after:bg-gray-primary/60 after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:z-40">
-        {/* TODO: inserir a imagem por enquanto depois pegar e colocar o video */}
+      <section className="relative z-30 w-full h-[60vh] max-h-[720px] min-h-[420px] rounded-[30px] overflow-hidden flex items-center justify-center lg:rounded-[60px] after:content-[''] after:w-full after:h-full after:bg-gray-primary/40 after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:z-40">
+        
+        {/* IMAGEM */}
         <Image
           src={AboutImage}
-          alt="Exemplo"
+          alt="Sala Imersiva Hanazaki Studio"
           fill
           priority
           placeholder="blur"
           className="absolute top-0 left-0 right-0 bottom-0 w-full h-full object-cover object-center"
         />
 
+        {/* CONTEÚDO */}
         <motion.div
           role="button"
           onClick={OpenModal}
           initial="initial"
           whileInView="animate"
-          viewport={{ once: true, amount: "some" }}
+          viewport={{ once: true, amount: 0.4 }}
           variants={ContainerAnimation}
           className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-50 flex flex-col space-y-6 items-center justify-center group cursor-pointer overflow-hidden"
         >
-          <motion.div variants={LineAnimation} className="w-full h-[2px] min-h-[2px] bg-gold-primary self-start" />
+          
+          {/* LINHA SUPERIOR */}
+          <motion.div
+            variants={LineAnimation}
+            className="w-full max-w-[220px] h-[2px] bg-gold-primary"
+          />
 
+          {/* TEXTO */}
           <motion.h2
             variants={TextAnimation}
             className="text-2xl text-center uppercase text-light-primary font-medium sm:text-5xl lg:text-6xl"
           >
-            Por dentro da <strong className="font-medium text-gold-primary">sala imersiva</strong>
+            Por dentro da{" "}
+            <strong className="font-medium text-gold-primary">
+              sala imersiva
+            </strong>
           </motion.h2>
 
+          {/* PLAY */}
           <motion.div variants={PlayAnimation}>
             <PlayCircle
-              size="67px"
-              strokeWidth={1.5}
-              className="text-light-primary group-hover:text-gold-primary transition-colors"
+              size="64px"
+              strokeWidth={1.4}
+              className="text-light-primary group-hover:text-gold-primary transition-colors duration-300"
             />
           </motion.div>
 
-          <motion.div variants={LineAnimation} className="w-full h-[2px] min-h-[2px] bg-gold-primary self-end" />
+          {/* LINHA INFERIOR */}
+          <motion.div
+            variants={LineAnimation}
+            className="w-full max-w-[220px] h-[2px] bg-gold-primary"
+          />
         </motion.div>
       </section>
     </>
