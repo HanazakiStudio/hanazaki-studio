2fb820f24ad36e6b73b4c362e638c4c9427e8550
HanazakiStudio <leonardo@hanazakistudio.com>
2026-05-01 17:21:27 -0300

    Update page.tsx

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -5,6 +5,7 @@ import { LogosBanner } from "@/app/(website)/_components/logos-banner";
 import { VideoAbout } from "@/app/(website)/_components/video-about";
 import { About } from "@/app/(website)/_components/about";
 import { Services } from "@/app/(website)/_components/services";
+import { Chicago } from "@/app/(website)/_components/chicago";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <VideoAbout />
       <About />
       <Services />
+      <Chicago />
     </>
   );
 }
