c4b3b905142930e77d2ec86b48513bc55c511826
HanazakiStudio <leonardo@hanazakistudio.com>
2026-05-03 16:15:27 -0300

    Update page.tsx

--- app/(website)/sobre/page.tsx
+++ app/(website)/sobre/page.tsx
@@ -1,14 +1,11 @@
 import { AboutBanner } from "./_components/about-banner";
 import { AboutContent } from "./_components/about-content";
-import { AboutTrades } from "./_components/about-trades";
-import { AboutBio } from "./_components/about-bio";
 
 function AboutPage() {
   return (
     <>
       <AboutBanner />
       <AboutContent />
-      <AboutTrades />
     </>
   );
 }
