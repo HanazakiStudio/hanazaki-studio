f88f80a868abcf670db1ed1eca597c112f844af7
HanazakiStudio <leonardo@hanazakistudio.com>
2026-05-05 15:09:19 -0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,15 +7,16 @@ import { SocialMediaLink } from "./social-media-link";
 export function Footer() {
   return (
     <footer className="w-full mt-12 sm:mt-24">
-      <div className="w-full px-6 flex flex-col items-center gap-8 
-        sm:flex-row sm:items-stretch sm:justify-between sm:px-16 
-        lg:container lg:mx-auto">
-
+      <div
+        className="w-full px-6 flex flex-col items-center gap-10
+        sm:flex-row sm:items-stretch sm:justify-between sm:px-16
+        lg:container lg:mx-auto"
+      >
         {/* LOGO */}
-        <div className="flex items-center justify-center">
+        <div className="flex items-start justify-center">
           <Link
             href="/"
-            className="relative w-28 h-28 sm:w-36 sm:h-32 lg:w-48 lg:h-48"
+            className="relative w-28 h-28 sm:w-36 sm:h-32 lg:w-44 lg:h-44"
           >
             <Image
               src="/images/logo-white.svg"
@@ -30,7 +31,7 @@ export function Footer() {
         <div className="hidden sm:block w-px bg-gold-primary" />
 
         {/* FORM */}
-        <div className="flex-1 max-w-xl w-full">
+        <div className="flex-1 max-w-lg w-full">
           <FooterForm />
         </div>
 
@@ -38,7 +39,7 @@ export function Footer() {
         <div className="hidden sm:block w-px bg-gold-primary" />
 
         {/* SOCIAL */}
-        <div className="flex flex-col justify-center gap-10 w-full sm:w-auto">
+        <div className="flex flex-col justify-start gap-12 w-full sm:w-auto pt-[6px]">
           <SocialMediaLink
             href="https://wa.me/5516997054012?text=Ol%C3%A1%2C+gostaria+de+saber+mais+sobre+o+seu+servi%C3%A7o."
             alt="Whatsapp"
@@ -63,10 +64,11 @@ export function Footer() {
       </div>
 
       {/* BOTTOM */}
-      <div className="w-full mt-12 py-6 border-t border-light-primary px-6 
-        flex flex-col items-center gap-4 
-        sm:flex-row sm:justify-between sm:px-16">
-
+      <div
+        className="w-full mt-12 py-6 border-t border-light-primary px-6
+        flex flex-col items-center gap-4
+        sm:flex-row sm:justify-between sm:px-16"
+      >
         <span className="poppins-font text-sm text-light-primary font-medium sm:text-base">
           Hanazaki Studio ©2024
         </span>
