52fb3a049d64fda7b8d008e821af01dff56693b9
HanazakiStudio <leonardo@hanazakistudio.com>
2026-05-04 23:44:37 -0300

    Update about-content.tsx

--- app/(website)/sobre/_components/about-content.tsx
+++ app/(website)/sobre/_components/about-content.tsx
@@ -22,7 +22,7 @@ export function AboutContent() {
         variants={BelowContainerAnimation}
         className="w-full mt-16 mb-12 sm:mt-24"
       >
-        <div className="relative w-full flex flex-col items-center justify-center min-h-[260px] sm:min-h-[300px] lg:min-h-[340px]">
+        <div className="relative w-full flex flex-col items-center justify-center min-h-[380px] sm:min-h-[440px] lg:min-h-[500px]">
           <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
             <Image
               src="/images/about-international.svg"
@@ -31,7 +31,7 @@ export function AboutContent() {
               width={520}
               height={480}
               priority
-              className="object-contain w-auto h-[210px] sm:h-[240px] lg:h-[260px]"
+              className="object-contain w-auto h-[320px] sm:h-[360px] lg:h-[390px]"
             />
           </div>
 
